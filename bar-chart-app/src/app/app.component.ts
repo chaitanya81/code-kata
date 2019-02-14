37db9270b6f2d8a3cbf3a8f834372b00ebcd04d1
anusha <anusha.kpf@gmail.com>
2019-02-14 17:24:57 -0500

    fixed unit tests and created bar graph

--- bar-chart-app/src/app/app.component.ts
+++ bar-chart-app/src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ChartDataService } from './chart-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['../../node_modules/nvd3/build/nv.d3.css']
 })
-export class AppComponent {
-  title = 'bar-chart-app';
+export class AppComponent implements OnInit {
+
+  barChartOptions = {
+    chart: {
+      color: function () { return "blue"; },
+      type: 'discreteBarChart',
+      height: 300,
+      width: 1000,
+      margin: {
+        top: 20,
+        right: 20,
+        bottom: 50,
+        left: 55
+      },
+      x: function (d) { return d.label; },
+      y: function (d) { return d.value; },
+      showValues: true,
+      valueFormat: function (d) {
+        return d3.format(',.0f')(d);
+      },
+      duration: 500,
+      xAxis: {
+        axisLabel: 'Each Day'
+      },
+      yAxis: {
+        axisLabel: 'Completed Tasks',
+        axisLabelDistance: -10
+      }
+    }
+  };
+
+
+  barChartData = [];
+
+  constructor(private chartDataService: ChartDataService) {
+  }
+
+  ngOnInit() {
+    this.chartDataService.getData().subscribe(
+      data => {
+        this.barChartData = [{
+          key: 'Cumulative Return',
+          values: data as any
+        }];
+      }, err => {
+        console.log(err);
+      });
+  }
 }
--- bar-chart-app/src/app/app.module.ts
+++ bar-chart-app/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NvD3Module } from 'ng2-nvd3';
+import { HttpClientModule } from '@angular/common/http';
+import { ChartDataService } from './chart-data.service';
+import 'd3';
+import 'nvd3';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NvD3Module,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ChartDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
